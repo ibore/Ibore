e989509c4c7a7fc98cf7c8491b478c92495e11dc
Ibore Xie <bore521@live.com>
2017-06-01 07:53:02 +0800

    0601

---
+++ http/src/main/java/me/ibore/http/HttpHeaders.java
@@ -0,0 +1,16 @@
+package me.ibore.http;
+
+/**
+ * description:
+ * author: Ibore Xie
+ * date: 2017-05-31 19:40
+ * website: ibore.me
+ */
+
+public final class HttpHeaders {
+
+
+    public static final class Builder {
+
+    }
+}
